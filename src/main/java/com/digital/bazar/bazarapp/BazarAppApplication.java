ec93cfe47d32108e821bf1dbcf649a4ea69f385b
Murali Boyapati <murali.boyapati@epsilon.com>
2019-01-21 13:59:32 -0600

    Shell

---
+++ src/main/java/com/digital/bazar/bazarapp/BazarAppApplication.java
@@ -0,0 +1,14 @@
+package com.digital.bazar.bazarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazarAppApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/digital/bazar/bazarapp/BazarAppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.digital.bazar.bazarapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BazarAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
